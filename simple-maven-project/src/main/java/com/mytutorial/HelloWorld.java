ce51ceead10cf38e0c1189c04638ec05e178d970
nelpintech <52632059+nelpintech@users.noreply.github.com>
2019-07-07 12:38:15 -0700

    Changed by user 2
    Changed by user 2

--- simple-maven-project/src/main/java/com/mytutorial/HelloWorld.java
+++ simple-maven-project/src/main/java/com/mytutorial/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("My first java code from a Maven based java program");
+		System.out.println("Changed in Git Hub");
 	}
 
 }
