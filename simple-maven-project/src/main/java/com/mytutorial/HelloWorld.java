506def66faca1cc969f7e36f8e0edb13f9a3ddd1
Nelson <nelsonpinto111@gmail.com>
2019-07-07 12:29:20 -0700

    user 1 - initial commit

---
+++ simple-maven-project/src/main/java/com/mytutorial/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.mytutorial;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My first java code from a Maven based java program");
+	}
+
+}
